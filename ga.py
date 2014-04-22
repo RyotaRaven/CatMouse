a898c233ad9a838738062b802346a4977428fc59
dhagandev <dhagan@utexas.edu>
2014-04-22 16:10:35 -0500

    played with ga. No one likes ga.

--- ga.py
+++ ga.py
@@ -9,34 +9,34 @@ from brain_dna import dna
 # evaluate
 
 population = []
-pop_size = 100
-generations = 300
+pop_size = 10
+generations = 50
 
 for x in range(pop_size):
- population.append( dna() )
+    population.append( dna() )
 
 best=None
 for generation in range(generations):
- for individual in population:
-  individual.evaluate()
- population.sort(key=lambda x:x.fitness,reverse=True)
- population=population[0:pop_size/2]
- best=population[0]
- print generation, population[0].fitness
- new_population = [best]
- for x in range(1,pop_size):
-  if random.random()<0.5:
-   child=random.choice(population).mutate()
-   new_population.append(child)
-  else:
-   p1 = random.choice(population)
-   p2 = random.choice(population)
-   child=p1.crossover(p2)
-   new_population.append(child)
- population=new_population
+    for individual in population:
+        individual.evaluate()
+    population.sort(key=lambda x:x.fitness,reverse=True)
+    population=population[0:pop_size/2]
+    best=population[0]
+    print generation, population[0].fitness
+    new_population = [best]
+    for x in range(1,pop_size):
+    if random.random()<0.5:
+        child=random.choice(population).mutate()
+    new_population.append(child)
+    else:
+        p1 = random.choice(population)
+        p2 = random.choice(population)
+        child=p1.crossover(p2)
+        new_population.append(child)
+    population=new_population
 
 
-from Bricks import game_function
+from catandmouse import game_function
 ann=best.create_neuron()
 print game_function(ann,True)
 
